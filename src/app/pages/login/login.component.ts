2ce3d962b9bc12c37e0dd97ed0f26d16c95b5684
andreylpereira <andreylpereira@gmail.com>
2022-01-26 13:28:59 -0300

    Correções em variaveis, breadcrumb e criação do grid-layout base das paginas

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,5 +10,4 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
