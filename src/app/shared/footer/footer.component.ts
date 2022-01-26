fe2d56efaf292b6fdf71c1057bdf3e78f6f3f878
andreylpereira <andreylpereira@gmail.com>
2022-01-25 23:41:59 -0300

    Correções no header, criação footer, adição de variaveis e correção em pages

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  email: string = 'contratech@gmail.com';
+  appName: string = 'Contratech 2022';
   constructor() { }
 
   ngOnInit(): void {
