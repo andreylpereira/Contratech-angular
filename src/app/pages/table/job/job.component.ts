b9720922ac44c40c888b3f0088f0572f53e6f25e
andreylpereira <andreylpereira@gmail.com>
2022-01-31 22:38:56 -0300

    Funcionalidades de deleção e adição de serviços na page table

--- src/app/pages/table/job/job.component.ts
+++ src/app/pages/table/job/job.component.ts
@@ -6,26 +6,62 @@ import { JobService } from 'src/app/services/job/job.service';
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
-  styleUrls: ['./job.component.css']
+  styleUrls: ['./job.component.css'],
 })
 export class JobComponent implements OnInit {
-
   @Input()
   etapaId: number | undefined;
   obraId: any;
   listJobs: Job[] = [];
 
-  displayedColumns: string[] = ['Serviço', 'Preço', 'Quantidade', 'Porcentagem', 'id'];
+  displayedColumns: string[] = [
+    'Serviço',
+    'Preço',
+    'Quantidade',
+    'Porcentagem',
+    'id',
+  ];
   columnsToDisplay = ['Serviço', 'Preço', 'Quantidade', 'Porcentagem', 'id'];
 
   constructor(private jobService: JobService, public route: ActivatedRoute) {
     this.obraId = this.route.snapshot.paramMap.get('id');
-   }
-
-  ngOnInit(): void {
-  this.jobService.getJobs(this.obraId, this.etapaId).subscribe((data) => (this.listJobs = data))
   }
 
+  ngOnInit(): void {
+    this.jobService
+      .getJobs(this.obraId, this.etapaId)
+      .subscribe((data) => (this.listJobs = data));
+  }
 
+  createJob() {
+    return this.jobService
+      .addJob(this.obraId, this.etapaId)
+      .subscribe((result) => {
+        result = this.jobService
+          .getJobs(this.obraId, this.etapaId)
+          .subscribe((newList) => (this.listJobs = newList));
+      });
+  }
+
+  deleteJob(id: number) {
+    return this.jobService
+      .delJob(this.obraId, this.etapaId, id)
+      .subscribe((result) => {
+        this.listJobs = [];
+        result = this.jobService
+          .getJobs(this.obraId, this.etapaId)
+          .subscribe((newList) => (this.listJobs = newList));
+      });
+  }
+
+  deleteAllJob() {
+    return this.jobService
+      .delAllJob(this.obraId, this.etapaId)
+      .subscribe((result) => {
+        this.listJobs = [];
+        result = this.jobService
+          .getJobs(this.obraId, this.etapaId)
+          .subscribe((newList) => (this.listJobs = newList));
+      });
+  }
 }
-
--- src/app/services/job/job.service.ts
+++ src/app/services/job/job.service.ts
@@ -4,7 +4,7 @@ import Job from 'src/app/models/job.model';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JobService {
   private userId: number = 0;
@@ -15,15 +15,53 @@ export class JobService {
     this.userToken = this.userService.getUser().token;
   }
 
+
   getJobs(idObra: any, idEtapa: any) {
     return this.http.get<Array<Job>>(
       `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos`,
       {
         headers: {
           'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + this.userToken,
+          Authorization: `Bearer ${this.userToken}`,
         },
       }
-    );
-  }
+      );
+    }
+
+    addJob(idObra: any, idEtapa: any) {
+      return this.http.post<any>(
+        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos`,
+        null,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.userToken}`,
+          },
+        },
+      );
+    }
+
+    delJob(idObra: any, idEtapa: any, idServico: any) {
+      return this.http.delete<any>(
+        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/${idServico}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.userToken}`,
+          },
+        }
+        );
+    }
+
+    delAllJob(idObra: any, idEtapa: any) {
+      return this.http.delete<any>(
+        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${this.userToken}`,
+          },
+        }
+        );
+    }
 }
