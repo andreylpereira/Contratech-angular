3266ec7bbfd56164b09a346d1869795141c06e58
andreylpereira <andreylpereira@gmail.com>
2022-02-02 15:19:02 -0300

    Funcionalidades da page jobs funcionais, sem validações e falta limpeza de codigo

--- src/app/pages/table/job/job.component.ts
+++ src/app/pages/table/job/job.component.ts
@@ -1,78 +1,93 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 /* Services */
 import { JobService } from 'src/app/services/job/job.service';
 
-import Job from 'src/app/models/job.model';
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
   styleUrls: ['./job.component.css'],
 })
 export class JobComponent implements OnInit {
+
   @Input()
   etapaId: number | undefined;
   obraId: any;
-  listJobs: Job[] = [];
-
-  displayedColumns: string[] = [
-    'Serviço',
-    'Preço',
-    'Quantidade',
-    'Porcentagem',
-    'id',
-  ];
-  columnsToDisplay = ['Serviço', 'Preço', 'Quantidade', 'Porcentagem', 'id'];
+  public jobsForm: any;
+  public listJobs: any;
 
   constructor(
     private jobService: JobService,
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     public route: ActivatedRoute
   ) {
     this.obraId = this.route.snapshot.paramMap.get('id');
+    this.jobsForm = this.fb.group({
+      jobs: this.fb.array([]),
+    });
   }
 
-  ngOnInit(): void {
-    this.jobService
-      .getJobs(this.obraId, this.etapaId)
-      .subscribe((data) => (this.listJobs = data));
+  ngOnInit() {
+    this.jobService.getJobs(this.obraId, this.etapaId).subscribe((data) => {
+      this.listJobs = data;
+
+      this.addListJobs(data.length);
+    });
   }
 
-  createJob() {
-    return this.jobService
-      .addJob(this.obraId, this.etapaId)
-      .subscribe((result) => {
-        result = this.jobService
-          .getJobs(this.obraId, this.etapaId)
-          .subscribe((newList) => (this.listJobs = newList));
+  /*
+   ! Função que transforma todos os dados recebidos do banco de dados em FormGroup para posteriormente serem inseridos no FormArray ("jobsForm")
+   ? Obs: Necessário desenvolver as validações necessárias.
+   */
+  addListJobs(x: number) {
+    for (let i = 0; i < x; i++) {
+      let newUsergroup: FormGroup = this.fb.group({
+        id: [this.listJobs[i].id],
+        nomeServico: [this.listJobs[i].nomeServico],
+        preco: [this.listJobs[i].preco],
+        quantidade: [this.listJobs[i].quantidade],
+        porcentagem: [this.listJobs[i].porcentagem],
       });
+      this.jobsForm.controls.jobs.push(newUsergroup);
+    }
   }
 
   deleteJob(id: number) {
     return this.jobService
       .delJob(this.obraId, this.etapaId, id)
-      .subscribe((result) => {
-        this.listJobs = [];
-        result = this.jobService
-          .getJobs(this.obraId, this.etapaId)
-          .subscribe((newList) => (this.listJobs = newList));
+      .subscribe(() => {
+        this.jobsForm.controls.jobs.clear();
+        this.ngOnInit();
       });
+  }
+
+  createJob() {
+    return this.jobService.addJob(this.obraId, this.etapaId).subscribe(() => {
+      this.jobsForm.controls.jobs.clear();
+      this.ngOnInit();
+    });
   }
 
   deleteAllJob() {
     return this.jobService
       .delAllJob(this.obraId, this.etapaId)
-      .subscribe((result) => {
-        this.listJobs = [];
-        result = this.jobService
-          .getJobs(this.obraId, this.etapaId)
-          .subscribe((newList) => (this.listJobs = newList));
+      .subscribe(() => {
+        this.jobsForm.controls.jobs.clear();
+        this.ngOnInit();
       });
   }
 
-  click() {
-    console.log(this.listJobs);
+  updateAllJobs() {
+    const data = this.jobsForm.controls.jobs['value'];
+    console.log(data);
+
+    return this.jobService
+      .putAllJob(this.obraId, this.etapaId, data)
+      .subscribe(() => {
+        this.jobsForm.controls.jobs.clear();
+        this.ngOnInit();
+      });
   }
 }
--- src/app/services/job/job.service.ts
+++ src/app/services/job/job.service.ts
@@ -15,7 +15,6 @@ export class JobService {
     this.userToken = this.userService.getUser().token;
   }
 
-
   getJobs(idObra: any, idEtapa: any) {
     return this.http.get<Array<Job>>(
       `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos`,
@@ -25,43 +24,56 @@ export class JobService {
           Authorization: `Bearer ${this.userToken}`,
         },
       }
-      );
-    }
+    );
+  }
 
-    addJob(idObra: any, idEtapa: any) {
-      return this.http.post<any>(
-        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos`,
-        null,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.userToken}`,
-          },
+  addJob(idObra: any, idEtapa: any) {
+    return this.http.post<any>(
+      `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos`,
+      null,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.userToken}`,
         },
-      );
-    }
+      }
+    );
+  }
 
-    delJob(idObra: any, idEtapa: any, idServico: any) {
-      return this.http.delete<any>(
-        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/${idServico}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.userToken}`,
-          },
-        }
-        );
-    }
+  delJob(idObra: any, idEtapa: any, idServico: any) {
+    return this.http.delete<any>(
+      `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/${idServico}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.userToken}`,
+        },
+      }
+    );
+  }
 
-    delAllJob(idObra: any, idEtapa: any) {
-      return this.http.delete<any>(
-        `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.userToken}`,
-          },
-        }
-        );
-    }
+  delAllJob(idObra: any, idEtapa: any) {
+    return this.http.delete<any>(
+      `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.userToken}`,
+        },
+      }
+    );
+  }
+
+  putAllJob(idObra: any, idEtapa: any, data: any) {
+    return this.http.put<any>(
+      `/api/api/usuarios/${this.userId}/obras/${idObra}/etapas/${idEtapa}/servicos/atualizar`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.userToken}`,
+        },
+      }
+    );
+  }
 }
